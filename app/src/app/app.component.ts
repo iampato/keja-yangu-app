e471752924f1c42a722c4de520524e30f31a0798
Iampato <858wpwaweru@gmail.com>
2019-03-08 14:30:44 +0300

    Added login page to the app modules

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -5,6 +5,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { LoginPage } from '../pages/login/login';
+import { RegisterPage } from '../pages/register/register';
 
 @Component({
   templateUrl: 'app.html'
